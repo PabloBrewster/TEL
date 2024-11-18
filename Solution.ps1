6671a9c15db83cffaadd3cb01887ed2e59597f67
PabloBrewster <39218285+PabloBrewster@users.noreply.github.com>
2024-11-18 16:17:57 +0000

    Update Solution.ps1

--- Solution.ps1
+++ Solution.ps1
@@ -65,10 +65,6 @@ $sql = $response.Content
 Invoke-Sqlcmd -ServerInstance $SQLServerName".database.windows.net" -Database $SQLDatabaseName -AccessToken $access_token `
               -Query $sql
 
-# Simulate OLTP workload in the source SQL DB
-Invoke-Sqlcmd -ServerInstance $SQLServerName".database.windows.net" -Database $SQLDatabaseName `
--AccessToken $access_token -Query "EXECUTE dbo.CA_Benchmark @IO_Benchmark = 1, @StressLevel = 2, @Batches = 1;"
-
 # Deploy Data Factory Linked Services, Data Sets, Pipeline and Trigger
 Set-AzDataFactoryV2LinkedService -DataFactoryName $dataFactoryName 
     -ResourceGroupName $resourceGroupName -Name "ls_cdbapbdev01" 
@@ -99,12 +95,25 @@ Set-AzDataFactoryV2Trigger -DataFactoryName $dataFactoryName `
     -ResourceGroupName $resourceGroupName -Name "trpbhub00" `
     -DefinitionFile .\DataFactory\Triggers\trpbhub00.json
 
+# Simulate OLTP workload in the source SQL DB
+Invoke-Sqlcmd -ServerInstance $SQLServerName".database.windows.net" -Database $SQLDatabaseName `
+-AccessToken $access_token -Query "EXECUTE dbo.CA_Benchmark @IO_Benchmark = 1, @StressLevel = 2, @Batches = 1;"
+
 # Trigger pipeline to sync changes from SQL source to NoSQL sink
 Invoke-AzDataFactoryV2Pipeline -DataFactory $dataFactoryName `
     -PipelineName "pl_etl_Merkle" `
     -ResourceGroupName $resourceGroupName
 
-# Delete all Azure resources created and used in this execution. 
+# Simulate OLTP workload (inserts) in source SQL DB
+Invoke-Sqlcmd -ServerInstance $SQLServerName".database.windows.net" -Database $SQLDatabaseName `
+-AccessToken $access_token -Query "EXECUTE dbo.CA_Benchmark @IO_Benchmark = 1, @StressLevel = 2, @Batches = 1;"
+
+# Trigger pipeline to sync changes from SQL source to NoSQL sink (Inserts/Updates only, using timestamp > last data factory runtime)
+Invoke-AzDataFactoryV2Pipeline -DataFactory $dataFactoryName `
+    -PipelineName "pl_etl_Merkle" `
+    -ResourceGroupName $resourceGroupName
+
+# Delete all Azure resources created and used in this execution, the SQL DB source, CosmosDB sink, Data Factory & Resource Group. 
 Set-location ./terraform
 terraform destroy
 Set-Location ../
