740727382d6f3e2e128b806fee298a6a2dd0df12
PabloBrewster <39218285+PabloBrewster@users.noreply.github.com>
2024-11-18 19:27:35 +0000

    Create CheapETLForCA.sql

---
+++ SQL/CheapETLForCA.sql
@@ -0,0 +1,33 @@
+-- Load control table used for cheap incremental ETL solution  
+IF OBJECT_ID('load_control') IS NOT NULL
+	DROP TABLE dbo.load_control;
+GO
+
+CREATE TABLE dbo.load_control 
+(
+	id INT IDENTITY(1,1) PRIMARY KEY, 
+	load_start DATETIME, 
+	load_end DATETIME, 
+	row_count INT, 
+	load_status varchar(10), 
+	updated_at TIMESTAMP
+);
+GO
+
+-- Load control view used for cheap incremental ETL solution  
+IF EXISTS (SELECT * FROM sys.views WHERE [name] = 'vw_transform_merkle')
+	DROP VIEW dbo.vw_transform_merkle;
+GO
+
+CREATE VIEW [dbo].[vw_transform_merkle] AS
+SELECT m.ID, m.Pattern_ID, m.Session_ID, m.x, m.y, m.updated_at,
+	CAST('POLYGON( (' +
+		CAST((x+1.2) AS VARCHAR(7)) + ' ' + CAST((y+1.2) AS VARCHAR(7)) + ','  +
+		CAST((x) AS VARCHAR(7)) + ' ' + CAST((y+1.2) AS VARCHAR(7)) + ','  +
+		CAST((x) AS VARCHAR(7)) + ' ' + CAST((y) AS VARCHAR(7)) + ','  +
+		CAST((x+1.2) AS VARCHAR(7)) + ' ' + CAST((y) AS VARCHAR(7)) + ','  +
+		CAST((x+1.2) AS VARCHAR(7)) + ' ' + CAST((y+1.2) AS VARCHAR(7)) +
+	') )' AS GEOMETRY) AS grid_reference
+FROM dbo.merkle m
+WHERE m.updated_at > (SELECT ISNULL(MAX(updated_at),0) FROM dbo.load_control WHERE load_status NOT IN ('Failed','Running'));
+GO
